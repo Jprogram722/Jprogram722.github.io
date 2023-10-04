0f8340bf59607c5218f2858a0c07e5215fcde563
Jared <PJared722@gmail.com>
2023-10-03 22:09:20 -0300

    Added animations and changed around some links to fit W3C standard

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
 /**
  * Auhtor: Jared Park
  */
-
-function removeActiveClass() {
-    navLinks.forEach((navLink) => {
-        console.log(navLink.classList);
-        navLink.classList.remove("active");
-    })
-}
-
-function addActiveClass(entries, observer) {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            removeActiveClass();
-            document.querySelector("a[href*='#"+entry.target.id+"']").classList.add('active');
-        }
-    })
-}
-
-const options = {
-    threshold: 0.8,
-};
-
-const sections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('.navlinks');
-console.log(navLinks);
-
-const observer = new IntersectionObserver(addActiveClass, options);
-
-sections.forEach((section) => {
-    observer.observe(section);
-})
