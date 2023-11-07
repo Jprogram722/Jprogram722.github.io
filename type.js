d81208a25d3e6f4b6618ad49261cf6fa25ccf5f9
Jared <PJared722@gmail.com>
2023-11-07 10:52:38 -0400

    fixed a clipping issue and merge blinker and type.js

--- type.js
+++ type.js
@@ -5,6 +5,9 @@
     page
 */
 
+// grab the input indicator element from the dom
+const inputIndicator = document.querySelector('#input-indicator');
+
 // grab the phrase element from the dom
 const phraseElement = document.querySelector('#phrase');
 
@@ -14,17 +17,44 @@ const phrase = "Finding The Solutions In Data";
 // init a variable for how many letters have been typed and index of that letter in the string
 let typedPhrase = "";
 let i = 0;
+let present = true;
 
 // this function will grab the letter of the corrisponding index in the phrase
 function getLetter(pharse, index){
     return pharse[index];
 }
 
-// sets it so that each letter is typed after every 100 milliseconds
-setInterval(() => {
+function typeLetter() {
     if(i < phrase.length){
+        // gets a letter
         typedPhrase += getLetter(phrase, i);
+        // types the letter in to html
         phraseElement.textContent = typedPhrase;
         i += 1;
     }
-}, 100);
+    else{
+
+        // stops the interval for typing
+        clearInterval(typeInterval);
+
+        // sets it so that the cursor blinks after every 530 milliseconds
+        setInterval(blink, 530);
+    }
+}
+
+function blink() {
+
+    if(present){
+        // hide the indicator
+        inputIndicator.style.visibility = 'hidden';
+    }
+    else{
+        // show the indicator
+        inputIndicator.style.visibility = '';
+    }
+
+    present = !present;
+}
+
+// sets it so that each letter is typed after every 100 milliseconds
+let typeInterval = setInterval(typeLetter, 100);
